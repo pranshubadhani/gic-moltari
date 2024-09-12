ce2fe26ff1f05939710982eacca7dcf2aff26ea8
pranshubadhani <pranshubadhani@gmail.com>
2024-09-12 18:18:10 +0530

    Intialisation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <section className="p-8 text-center">
+        <p className="text-lg">Home page content goes here.</p>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
